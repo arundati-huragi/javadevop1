65642622016f2d0411d354a05c182152e71d73b1
arundati-huragi <arundatikhuragi@gmail.com>
2025-04-09 15:22:41 +0530

    my add separate file

---
+++ Add.java
@@ -0,0 +1,15 @@
+package cie1;
+
+public class Add {
+
+        public static void main(String[] args) {
+            int a=30;
+            int b=25;
+            int sum=a+b;
+            System.out.println("the sum is:"+sum);
+    
+    }
+    }
+    
+
+
