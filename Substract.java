d2712cb1b8022406e576850143b44ca4c94fa1c7
arundati-huragi <arundatikhuragi@gmail.com>
2025-04-09 15:30:24 +0530

    my cie substract file

---
+++ Substract.java
@@ -0,0 +1,11 @@
+package cie1;
+
+public class Substract {
+    public static void main(String[] args) {
+        int a=30;
+        int b=25;
+        int sub=a-b;
+        System.out.println("the sum is:"+sub);
+    }
+
+}
